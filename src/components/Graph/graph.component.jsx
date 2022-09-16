9a8aca650d5a69b7e6f9ede102caf185e0f90dd4
Jay <shahjay2111@gmail.com>
2022-09-16 15:39:51 -0400

    Created a bar graph specific to location selected

--- src/components/Graph/graph.component.jsx
+++ src/components/Graph/graph.component.jsx
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from "react";
 import { fetchDailyData } from "../../api";
 import { Line, Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement } from 'chart.js'; 
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Filler, BarElement, Legend } from 'chart.js'; 
 import styles from './graph.module.css';
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement );
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Filler, BarElement, Legend );
 
-const Graph = ({ data, country }) => {
+const Graph = ({ data : {confirmed, deaths}, country }) => {
     const [dailyData, setDailyData] = useState([]);
 
     useEffect(() => {
@@ -14,9 +14,7 @@ const Graph = ({ data, country }) => {
             setDailyData(await fetchDailyData());
         }
         fetchAPI();
-    });
-
-    
+    }, []);
 
     const lineGraph = (
         dailyData[0]
@@ -41,9 +39,33 @@ const Graph = ({ data, country }) => {
         )  : null
     );
 
+    console.log(confirmed); 
+
+    const barGraph = (
+        confirmed ? (
+          <Bar
+            data={{
+              labels: ['Infected', 'Deaths'],
+              datasets: [
+                {
+                  label: 'People',
+                  backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+                  data: [confirmed.value, deaths.value],
+                },
+              ],
+            }}
+            options={{
+              legend: { display: false },
+              title: { display: true, text: `Current state in ${country}` },
+            }}
+          />
+        ) : null
+      );
+
     return(
         <div className={styles.container}>
-            {lineGraph} 
+            {country ? barGraph : lineGraph} 
+            {/* {lineGraph} */}
         </div>
     );
 }
